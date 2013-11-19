d435da21603210bd683b0dde9be86507aa2f1c75
ajlopez <webmaster@ajlopez.com>
2013-11-19 19:27:01 -0300

    Get name

--- test/parser.js
+++ test/parser.js
@@ -24,3 +24,27 @@ exports['Get integer with spaces'] = function (test) {
     
     test.equal(parser.parse("Integer"), null);
 }
+
+exports['Get name'] = function (test) {
+    var parser = parsers.createParser("name");
+    
+    var result = parser.parse("Name");
+    
+    test.ok(result);
+    test.equal(result.value, "name");
+    test.equal(result.type, "Name");
+    
+    test.equal(parser.parse("Name"), null);
+}
+
+exports['Get name with spcaes'] = function (test) {
+    var parser = parsers.createParser("  name   ");
+    
+    var result = parser.parse("Name");
+    
+    test.ok(result);
+    test.equal(result.value, "name");
+    test.equal(result.type, "Name");
+    
+    test.equal(parser.parse("Name"), null);
+}
