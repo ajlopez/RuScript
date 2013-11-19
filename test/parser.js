68b2dc4ce2c1a62cd40cd869f04acf484500a1e0
ajlopez <webmaster@ajlopez.com>
2013-11-19 19:21:17 -0300

    Get integer skipping spaces

--- test/parser.js
+++ test/parser.js
@@ -12,3 +12,15 @@ exports['Get integer'] = function (test) {
     
     test.equal(parser.parse("Integer"), null);
 }
+
+exports['Get integer with spaces'] = function (test) {
+    var parser = parsers.createParser("  123   s");
+    
+    var result = parser.parse("Integer");
+    
+    test.ok(result);
+    test.equal(result.value, "123");
+    test.equal(result.type, "Integer");
+    
+    test.equal(parser.parse("Integer"), null);
+}
