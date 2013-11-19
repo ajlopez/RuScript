1fd951aa3912cf62b1b39646fdb6b3615bdf37c7
ajlopez <webmaster@ajlopez.com>
2013-11-19 19:55:00 -0300

    Add expression

--- test/evaluate.js
+++ test/evaluate.js
@@ -11,3 +11,14 @@ exports['Evaluate integer'] = function (test) {
     
     test.equal(parser.parse("Integer"), null);
 };
+
+exports['Evaluate add integers'] = function (test) {
+    var parser = parsers.createParser("1+2");
+    var expr = parser.parse("Expression");
+    var result = expr.value.evaluate(null);
+    
+    test.ok(result);
+    test.equal(result, 3);
+    
+    test.equal(parser.parse("Expression"), null);
+};
