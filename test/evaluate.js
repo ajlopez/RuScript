f5203e804896bb2d08d5f6627e0e2eca9d46cea7
ajlopez <webmaster@ajlopez.com>
2014-01-28 15:14:53 -0300

    Refactor evaluate test, WIP

--- test/evaluate.js
+++ test/evaluate.js
@@ -2,62 +2,43 @@
 var parsers = require('../lib/parsers'),
     contexts = require('../lib/contexts');
 
+function evaluate(text, term, context, test) {
+    var parser = parsers.createParser(text);
+    var expr = parser.parse(term);
+    var result = expr.value.evaluate(context);
+    test.equal(parser.parse(term), null);
+    return result;
+}
+    
 exports['Evaluate integer'] = function (test) {
-    var parser = parsers.createParser("123");
-    var expr = parser.parse("Integer");
-    var result = expr.value.evaluate(null);
+    var result = evaluate("123", "Integer", null, test);
     
     test.ok(result);
     test.equal(result, 123);
-    
-    test.equal(parser.parse("Integer"), null);
 };
 
 exports['Evaluate integer as term'] = function (test) {
-    var parser = parsers.createParser("123");
-    var expr = parser.parse("Term");
-    var result = expr.value.evaluate(null);
+    var result = evaluate("123", "Term", null, test);
     
     test.ok(result);
     test.equal(result, 123);
-    
-    test.equal(parser.parse("Term"), null);
-};
-
-exports['Evaluate integer as term'] = function (test) {
-    var parser = parsers.createParser("123");
-    var expr = parser.parse("Term");
-    var result = expr.value.evaluate(null);
-    
-    test.ok(result);
-    test.equal(result, 123);
-    
-    test.equal(parser.parse("Term"), null);
 };
 
 exports['Evaluate false'] = function (test) {
-    var parser = parsers.createParser("false");
-    var expr = parser.parse("Expression");
-    test.ok(expr);
-    test.strictEqual(expr.value.evaluate(null), false);
+    var result = evaluate("false", "Expression", null, test);
+    test.strictEqual(result, false);
 };
 
 exports['Evaluate true'] = function (test) {
-    var parser = parsers.createParser("true");
-    var expr = parser.parse("Expression");
-    test.ok(expr);
-    test.strictEqual(expr.value.evaluate(null), true);
+    var result = evaluate("true", "Expression", null, test);
+    test.strictEqual(result, true);
 };
 
 exports['Evaluate subtract integers'] = function (test) {
-    var parser = parsers.createParser("1-2");
-    var expr = parser.parse("Expression");
-    var result = expr.value.evaluate(null);
+    var result = evaluate("1-2", "Expression", null, test);
     
     test.ok(result);
     test.equal(result, -1);
-    
-    test.equal(parser.parse("Expression"), null);
 };
 
 exports['Evaluate multiply integers'] = function (test) {
