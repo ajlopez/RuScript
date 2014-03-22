f933406a9428b7b3a556f7055802379f281830c4
ajlopez <webmaster@ajlopez.com>
2014-03-22 20:16:30 -0300

    Removing file

--- test/class.js
+++ test/class.js
@@ -13,6 +13,15 @@ exports['empty class'] = function (test) {
     test.ok(result.$class.getInstanceMethod("new"));
 }
 
+exports['new object from empty class'] = function (test) {
+    var context = rs.createContext();
+    var result = rs.execute("class Dog\n\end\nDog.new", context);
+    
+    test.ok(result);
+    test.ok(result.$class)
+    test.strictEqual(result.$class, context.getValue("Dog"));
+}
+
 exports['class with def'] = function (test) {
     var context = rs.createContext();
     rs.execute("class Dog\n\def get_value\n42\end\nend", context);
