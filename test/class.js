fb52a1c5574f28cc3031fe6211efa18eeb7b07dd
ajlopez <webmaster@ajlopez.com>
2014-03-22 19:31:53 -0300

    Class with instance method

--- test/class.js
+++ test/class.js
@@ -13,10 +13,11 @@ exports['empty class'] = function (test) {
 
 exports['class with def'] = function (test) {
     var context = rs.createContext();
-    rs.execute("class Dog\n\def value\n42\end\nend", context);
+    rs.execute("class Dog\n\def get_value\n42\end\nend", context);
     
     var result = context.getLocalValue("Dog");
     
     test.ok(result);
     test.equal(result.getName(), "Dog");
+    test.ok(result.getInstanceMethod("get_value"));
 }
