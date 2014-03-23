7d89fa89bb09bd9ff1880bdddc5d0d6f56d8ee9b
ajlopez <webmaster@ajlopez.com>
2014-03-23 18:08:26 -0300

    Assert first sample

---
+++ samples/assert/arith.rb
@@ -0,0 +1,27 @@
+
+# integer operations
+
+assert 1==1
+assert 1+2 == 3
+assert 3-2 == 1
+assert 3*4 == 12
+assert 6/2 == 3
+
+# precedence
+
+assert 1+2*3 == 7
+assert 1 + 6/2 == 4
+
+# variables
+
+one = 1
+two = 2
+three = 3
+four = 4
+
+assert one==one
+assert one+two == three
+assert three-two == one
+assert three*four == 12
+assert 6/two == three
+
