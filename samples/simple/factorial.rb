6feb70065500e6b3dec9c80f8becde94eb950735
ajlopez <webmaster@ajlopez.com>
2014-05-04 16:55:20 -0300

    Samples WIP

--- samples/simple/factorial.rb
+++ samples/simple/factorial.rb
@@ -1,1 +1,13 @@
-def factorial(n)  if n == 1    1  else    n * factorial(n-1)  endendx = 1while x <= 10  puts factorial(x)  x = x + 1end
+def factorial(n)
+  if n == 1
+    1
+  else
+    n * factorial(n-1)
+  end
+end
+
+x = 1
+
+while x <= 10  puts factorial(x)
+  x = x + 1
+end
--- samples/simple/fibo.rb
+++ samples/simple/fibo.rb
@@ -1,1 +1,24 @@
-def fibo(n)  puts "n"  puts n    if n == 0    puts "zero"    1  else    if n == 1      puts "one"      1    else      puts n-1      puts n-2      fibo(n - 1) + fibo(n - 2)    end  endendx = 1while x <= 2  puts fibo(x)  x = x + 1end
+def fibo(n)
+  puts "n"
+  puts n
+  
+  if n == 0
+    puts "zero"
+    1
+  else
+    if n == 1
+      puts "one"
+      1
+    else      puts "other"      n1 = n-1      n2 = n-2
+      puts n1
+      puts n2      f1 = fibo(n1)      f2 = fibo(n2)      f1 + f2
+    end
+  end
+end
+
+x = 1
+
+while x <= 2
+  fibo(x)
+  x = x + 1
+end
