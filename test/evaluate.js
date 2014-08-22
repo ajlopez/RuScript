14e21a2ec2a7d74364693ec2708ef11a952106c2
ajlopez <webmaster@ajlopez.com>
2014-08-22 10:52:53 -0300

    Additional test, evaluate string

--- test/evaluate.js
+++ test/evaluate.js
@@ -17,6 +17,13 @@ exports['Evaluate integer'] = function (test) {
     test.equal(result, 123);
 };
 
+exports['Evaluate string'] = function (test) {
+    var result = evaluate('"foo"', "String", null, test);
+    
+    test.ok(result);
+    test.equal(result, "foo");
+};
+
 exports['Evaluate integer as term'] = function (test) {
     var result = evaluate("123", "Term", null, test);
     
