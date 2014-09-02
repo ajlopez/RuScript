7090e1784c932e35cf606274c2790e2554656eff
ajlopez <webmaster@ajlopez.com>
2014-09-02 07:29:34 -0300

    Fixing end of lines

--- samples/assert/run.js
+++ samples/assert/run.js
@@ -1,1 +1,20 @@
-var ruscript = require('../..'),    fs = require('fs');var text = fs.readFileSync(process.argv[2]).toString();var context = ruscript.createContext();context.setLocalValue('assert', function (test, msg) {    if (test)        return;            if (msg)        throw msg;            throw "assertion failure";});ruscript.execute(text, context);
+
+var ruscript = require('../..'),
+    fs = require('fs');
+
+var text = fs.readFileSync(process.argv[2]).toString();
+
+var context = ruscript.createContext();
+
+context.setLocalValue('assert', function (test, msg) {
+    if (test)
+        return;
+        
+    if (msg)
+        throw msg;
+        
+    throw "assertion failure";
+});
+
+ruscript.execute(text, context);
+
