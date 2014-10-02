1da2aae65123e9d35c10d70b32f337fda6c84fd8
ajlopez <webmaster@ajlopez.com>
2014-10-02 18:27:10 -0300

    Using puts in samples

--- samples/simple/factorial.rb
+++ samples/simple/factorial.rb
@@ -8,6 +8,6 @@ end
 
 x = 1
 
-while x <= 10  puts factorial(x)
+while x <= 10  puts x, factorial(x)
   x = x + 1
 end
--- samples/simple/run.js
+++ samples/simple/run.js
@@ -2,7 +2,6 @@
 var ruscript = require('../..');
 
 var context = ruscript.context();
-context.setLocalValue('puts', function (arg) { console.log(arg); return arg; });
 
 for (var k = 2; k < process.argv.length; k++)
     ruscript.executeFile(process.argv[k], context);
