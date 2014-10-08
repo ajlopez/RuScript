8742606cd30954b03b2fff2381c7199767444b17
ajlopez <webmaster@ajlopez.com>
2014-10-08 07:24:08 -0300

    Require with cache

--- test/require.js
+++ test/require.js
@@ -15,3 +15,14 @@ exports['execute native require'] = function (test) {
     
     test.strictEqual(require('http'), result);
 }
+
+exports['execute require only once'] = function (test) {
+    var context = rs.context();
+    context.setLocalValue("a", 0);
+    
+    rs.execute('require("test/inca.rb")', context);
+    rs.execute('require("test/inca.rb")', context);
+        
+    test.equal(1, context.getValue("a"));
+}
+
