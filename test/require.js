1d2f160fab13e30018dc2901291cd50192e0430e
ajlopez <webmaster@ajlopez.com>
2014-10-08 07:19:44 -0300

    Native require, first test

--- test/require.js
+++ test/require.js
@@ -8,3 +8,10 @@ exports['execute require'] = function (test) {
     
     test.equal(42, context.getValue("a"));
 }
+
+exports['execute native require'] = function (test) {
+    var context = rs.context();
+    var result = rs.execute('require("http")', context);
+    
+    test.strictEqual(require('http'), result);
+}
